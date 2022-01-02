96d4d76127c890f9a3c88b66e312072621fb01e2
Deus <kirill2689@gmail.com>
2022-01-02 16:18:01 +0300

    get_tags function added; index.html page changed; working page_index()

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from functions import *
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+    tags = get_tags()
+    return render_template("index.html", tags=tags)
 
 
 @app.route("/tag")
--- functions.py
+++ functions.py
@@ -1,1 +1,16 @@
 import json
+
+
+def get_tags():
+    with open("posts.json") as f:
+        raw_json = f.read()
+    posts = json.loads(raw_json)
+    tags = []
+    chars_to_strip = "#!?;:.,&*()"
+    for post in posts:
+        tags += [x.strip(chars_to_strip) for x in post['content'].split(" ") if x.startswith("#")]
+    return set(tags)
+
+
+
+# print(get_tags())
