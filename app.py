7c5fc6bed58898cf003cb1b251215cb048d7ca7a
Deus <kirill2689@gmail.com>
2022-01-02 20:16:57 +0300

    Added forms to add new posts

--- app.py
+++ app.py
@@ -9,21 +9,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    tags = get_tags()
+    tags = get_tags()  # Получаю тэги при каждом переходе на главную страницу
     return render_template("index.html", tags=tags)
 
 
 @app.route("/tag")
-def page_tag():
+def page_tag():  # Предполагаю, что проверка будет ненужной, если переходить по тэгам с главной страницы
     tag = '#' + request.args.get("tag")
-    print(tag)
     found_posts = search_tags(tag)
     return render_template("post_by_tag.html", tag=tag[1:].title(), posts=found_posts)
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
-    pass
+    if request.method == "GET":  # Вывод формы для поста
+        return render_template("post_form.html")
+    # Иначе обрабатываю пост
+    content = request.form.get("content")
+    pic = request.files['picture']
+    pic.save(UPLOAD_FOLDER + "/" + pic.filename)
+    post = {
+        "pic": UPLOAD_FOLDER + "/" + pic.filename,
+        "content": content
+    }
+    add_post(post)
+    return render_template("post_uploaded.html", post=post, upload=UPLOAD_FOLDER)
 
 
 @app.route("/uploads/<path:path>")
@@ -31,5 +41,5 @@ def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run(debug=True)
+app.run()
 
--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import json
 
 
-def get_tags():
+def get_tags():  # прохожу по списку постов, выделяю тэги
     with open("posts.json") as f:
         raw_json = f.read()
     posts = json.loads(raw_json)
@@ -11,7 +11,7 @@ def get_tags():
     return set(tags)
 
 
-def search_tags(tag):
+def search_tags(tag):  # прохожу по постам и ищу все посты, содержащие переданный тэг
     with open("posts.json") as f:
         raw_json = f.read()
     posts = json.loads(raw_json)
@@ -19,9 +19,17 @@ def search_tags(tag):
     for post in posts:
         if tag in post['content']:
             result.append(post)
-    print(result)
     return result
 
 
+def add_post(post):  # добавляю пост во файл posts.json
+    with open("posts.json") as f:
+        raw_json = f.read()
 
-print(get_tags())
+    posts = json.loads(raw_json)
+    posts.append(post)
+    raw_json = json.dumps(posts, ensure_ascii=False)
+
+    with open("posts.json", "w") as f:
+        f.write(raw_json)
+
